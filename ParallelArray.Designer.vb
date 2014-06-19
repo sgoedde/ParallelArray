bc63476907d593b9b5cb74aa52a35f14b7b64ec2
sgoedde <s.goedde.sg@gmail.com>
2014-06-18 22:01:43 -0500

    Initial Commit

---
+++ ParallelArray.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmParallelArray
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblSalary = New System.Windows.Forms.Label()
+        Me.txtBxName = New System.Windows.Forms.TextBox()
+        Me.txtBxSalary = New System.Windows.Forms.TextBox()
+        Me.txtBxOutput = New System.Windows.Forms.RichTextBox()
+        Me.txtBxNewOutput = New System.Windows.Forms.RichTextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblPercent = New System.Windows.Forms.Label()
+        Me.txtBxPercent = New System.Windows.Forms.TextBox()
+        Me.btnNewReport = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblStatus = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblStatusSpring = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lblNumRecords = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.txtBxHighLow = New System.Windows.Forms.RichTextBox()
+        Me.txtBxNewHighLow = New System.Windows.Forms.RichTextBox()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Location = New System.Drawing.Point(11, 9)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(44, 15)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Na&me:"
+        '
+        'lblSalary
+        '
+        Me.lblSalary.AutoSize = True
+        Me.lblSalary.Location = New System.Drawing.Point(90, 35)
+        Me.lblSalary.Name = "lblSalary"
+        Me.lblSalary.Size = New System.Drawing.Size(44, 15)
+        Me.lblSalary.TabIndex = 2
+        Me.lblSalary.Text = "&Salary:"
+        '
+        'txtBxName
+        '
+        Me.txtBxName.Location = New System.Drawing.Point(61, 6)
+        Me.txtBxName.Name = "txtBxName"
+        Me.txtBxName.Size = New System.Drawing.Size(180, 20)
+        Me.txtBxName.TabIndex = 1
+        '
+        'txtBxSalary
+        '
+        Me.txtBxSalary.Location = New System.Drawing.Point(140, 32)
+        Me.txtBxSalary.Name = "txtBxSalary"
+        Me.txtBxSalary.Size = New System.Drawing.Size(101, 20)
+        Me.txtBxSalary.TabIndex = 3
+        '
+        'txtBxOutput
+        '
+        Me.txtBxOutput.Font = New System.Drawing.Font("Courier New", 7.92!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBxOutput.Location = New System.Drawing.Point(12, 165)
+        Me.txtBxOutput.Name = "txtBxOutput"
+        Me.txtBxOutput.ReadOnly = True
+        Me.txtBxOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical
+        Me.txtBxOutput.Size = New System.Drawing.Size(483, 137)
+        Me.txtBxOutput.TabIndex = 12
+        Me.txtBxOutput.Text = ""
+        Me.txtBxOutput.WordWrap = False
+        '
+        'txtBxNewOutput
+        '
+        Me.txtBxNewOutput.Font = New System.Drawing.Font("Courier New", 7.92!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBxNewOutput.Location = New System.Drawing.Point(12, 376)
+        Me.txtBxNewOutput.Name = "txtBxNewOutput"
+        Me.txtBxNewOutput.ReadOnly = True
+        Me.txtBxNewOutput.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical
+        Me.txtBxNewOutput.Size = New System.Drawing.Size(483, 137)
+        Me.txtBxNewOutput.TabIndex = 14
+        Me.txtBxNewOutput.Text = ""
+        Me.txtBxNewOutput.WordWrap = False
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(268, 6)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(104, 23)
+        Me.btnAdd.TabIndex = 6
+        Me.btnAdd.Text = "&Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Location = New System.Drawing.Point(378, 6)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(104, 23)
+        Me.btnClear.TabIndex = 7
+        Me.btnClear.Text = "C&lear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Enabled = False
+        Me.btnCreate.Location = New System.Drawing.Point(268, 32)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(104, 23)
+        Me.btnCreate.TabIndex = 8
+        Me.btnCreate.Text = "&Create Report"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(378, 32)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(104, 23)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblPercent
+        '
+        Me.lblPercent.AutoSize = True
+        Me.lblPercent.Enabled = False
+        Me.lblPercent.Location = New System.Drawing.Point(5, 60)
+        Me.lblPercent.Name = "lblPercent"
+        Me.lblPercent.Size = New System.Drawing.Size(129, 15)
+        Me.lblPercent.TabIndex = 4
+        Me.lblPercent.Text = "Salary &Increase (in %):"
+        '
+        'txtBxPercent
+        '
+        Me.txtBxPercent.Enabled = False
+        Me.txtBxPercent.Location = New System.Drawing.Point(140, 57)
+        Me.txtBxPercent.Name = "txtBxPercent"
+        Me.txtBxPercent.Size = New System.Drawing.Size(101, 20)
+        Me.txtBxPercent.TabIndex = 5
+        '
+        'btnNewReport
+        '
+        Me.btnNewReport.Enabled = False
+        Me.btnNewReport.Location = New System.Drawing.Point(305, 61)
+        Me.btnNewReport.Name = "btnNewReport"
+        Me.btnNewReport.Size = New System.Drawing.Size(135, 24)
+        Me.btnNewReport.TabIndex = 10
+        Me.btnNewReport.Text = "Create &New Report"
+        Me.btnNewReport.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatus, Me.lblStatusSpring, Me.lblNumRecords})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 517)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(509, 22)
+        Me.StatusStrip1.TabIndex = 15
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblStatus
+        '
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(0, 17)
+        '
+        'lblStatusSpring
+        '
+        Me.lblStatusSpring.AutoSize = False
+        Me.lblStatusSpring.Name = "lblStatusSpring"
+        Me.lblStatusSpring.Size = New System.Drawing.Size(494, 17)
+        Me.lblStatusSpring.Spring = True
+        '
+        'lblNumRecords
+        '
+        Me.lblNumRecords.Name = "lblNumRecords"
+        Me.lblNumRecords.Size = New System.Drawing.Size(0, 17)
+        '
+        'txtBxHighLow
+        '
+        Me.txtBxHighLow.Font = New System.Drawing.Font("Courier New", 7.92!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBxHighLow.Location = New System.Drawing.Point(12, 97)
+        Me.txtBxHighLow.Name = "txtBxHighLow"
+        Me.txtBxHighLow.ReadOnly = True
+        Me.txtBxHighLow.Size = New System.Drawing.Size(256, 62)
+        Me.txtBxHighLow.TabIndex = 11
+        Me.txtBxHighLow.Text = ""
+        Me.txtBxHighLow.WordWrap = False
+        '
+        'txtBxNewHighLow
+        '
+        Me.txtBxNewHighLow.Font = New System.Drawing.Font("Courier New", 7.92!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBxNewHighLow.Location = New System.Drawing.Point(12, 308)
+        Me.txtBxNewHighLow.Name = "txtBxNewHighLow"
+        Me.txtBxNewHighLow.ReadOnly = True
+        Me.txtBxNewHighLow.Size = New System.Drawing.Size(256, 62)
+        Me.txtBxNewHighLow.TabIndex = 13
+        Me.txtBxNewHighLow.Text = ""
+        Me.txtBxNewHighLow.WordWrap = False
+        '
+        'frmParallelArray
+        '
+        Me.AcceptButton = Me.btnAdd
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(509, 539)
+        Me.Controls.Add(Me.txtBxNewHighLow)
+        Me.Controls.Add(Me.txtBxHighLow)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.btnNewReport)
+        Me.Controls.Add(Me.txtBxPercent)
+        Me.Controls.Add(Me.lblPercent)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtBxNewOutput)
+        Me.Controls.Add(Me.txtBxOutput)
+        Me.Controls.Add(Me.txtBxSalary)
+        Me.Controls.Add(Me.txtBxName)
+        Me.Controls.Add(Me.lblSalary)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "frmParallelArray"
+        Me.Text = "Parallel Array Sort"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblSalary As System.Windows.Forms.Label
+    Friend WithEvents txtBxName As System.Windows.Forms.TextBox
+    Friend WithEvents txtBxSalary As System.Windows.Forms.TextBox
+    Friend WithEvents txtBxOutput As System.Windows.Forms.RichTextBox
+    Friend WithEvents txtBxNewOutput As System.Windows.Forms.RichTextBox
+    Friend WithEvents btnAdd As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnCreate As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lblPercent As System.Windows.Forms.Label
+    Friend WithEvents txtBxPercent As System.Windows.Forms.TextBox
+    Friend WithEvents btnNewReport As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents lblStatus As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents lblNumRecords As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents lblStatusSpring As System.Windows.Forms.ToolStripStatusLabel
+    Friend WithEvents txtBxHighLow As System.Windows.Forms.RichTextBox
+    Friend WithEvents txtBxNewHighLow As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ ParallelArray.vb
@@ -0,0 +1,288 @@
+﻿'Stephanie Goedde
+'IT3013 VB
+'ParArray1
+'2-21-13
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmParallelArray
+
+    'class level variables
+    Public arrLast(49) As String
+    Public arrFirst(49) As String
+    Public arrMiddle(49) As String
+    Public arrSalary(49) As Double
+    Public intCount As Integer = 0
+    Public dblSum As Double
+
+    'gets name and salary from text boxes
+    'splits name into first, middle, and last then puts all into correct array
+    Private Sub btnAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAdd.Click
+        'variables
+        Dim strName, strLast, strFirst, strMiddle As String
+        Dim dblSalary As Double
+
+        Try
+            'get values from text boxes
+            strName = txtBxName.Text
+            dblSalary = Convert.ToDouble(txtBxSalary.Text) 'convert value to double
+
+            If strName.IndexOf(".") > -1 Then 'checks for period in strName
+                'strLast begins 2 indices after period
+                strLast = strName.Substring(strName.IndexOf(".") + 2)
+                'strFirst starts at 0 and goes until first space
+                strFirst = strName.Substring(0, strName.IndexOf(" "))
+                'strMiddle starts after first space and goes for 2 indices
+                strMiddle = strName.Substring(strName.IndexOf(" ") + 1, 2)
+            Else
+                'strLast begins 1 index after first space
+                strLast = strName.Substring(strName.IndexOf(" ") + 1)
+                'strFirst begins at 0 and goes until first space
+                strFirst = strName.Substring(0, strName.IndexOf(" "))
+                'strMiddle is empty
+                strMiddle = ""
+            End If
+
+            'name and salary are put into index intCount in arrays
+            arrLast(intCount) = strLast
+            arrFirst(intCount) = strFirst
+            arrMiddle(intCount) = strMiddle
+            arrSalary(intCount) = dblSalary
+            intCount += 1 'increase count
+            dblSum += dblSalary 'running sum of salaries
+
+            'update status labels-name and salary and the # of total records added
+            lblStatus.Text = "New Record Added: " & strName & " " & dblSalary.ToString("C2")
+            lblNumRecords.Text = "Total Records: " & intCount.ToString()
+
+            'clear txtBoxes and reset for new record
+            txtBxName.Clear()
+            txtBxSalary.Clear()
+            txtBxName.Focus()
+            btnCreate.Enabled = True 'allow for report
+
+            'after 50 records are entered give message saying no more records allowed
+            'disable new record controls
+            If intCount >= 50 Then
+                MessageBox.Show("Record limit reached. No more than 50 records permitted.", "Record limit reached", MessageBoxButtons.OK)
+                btnAdd.Enabled = False
+                lblName.Enabled = False
+                txtBxName.Enabled = False
+                lblSalary.Enabled = False
+                txtBxSalary.Enabled = False
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Invalid entry. Try again.", "Error")
+            txtBxName.Focus()
+        End Try
+
+    End Sub
+
+    Private Sub btnCreate_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCreate.Click
+        'variables
+        Dim dblAverage, dblHigh, dblLow As Double
+
+        Try
+            dblHigh = FindHigh(arrSalary) 'function to find highest salary
+            dblLow = FindLow(arrSalary) 'function to find lowest salary
+            dblAverage = dblSum / intCount 'calculates average salary
+
+            'method to sort arrays alphabetically by last name
+            Sort(arrLast, arrFirst, arrMiddle, arrSalary)
+
+            'prints built strings from functions into text boxes
+            txtBxHighLow.Text = OutputSalary(dblHigh, dblLow, dblAverage)
+            txtBxOutput.Text = PrintOutput(arrLast, arrFirst, arrMiddle, arrSalary, dblAverage)
+
+            'sets controls for increase salary function
+            lblPercent.Enabled = True
+            txtBxPercent.Enabled = True
+            txtBxPercent.Focus()
+            btnNewReport.Enabled = True
+
+            lblStatus.Text = String.Empty
+
+            btnAdd.Enabled = False
+            btnCreate.Enabled = False
+            lblName.Enabled = False
+            txtBxName.Enabled = False
+            lblSalary.Enabled = False
+            txtBxSalary.Enabled = False
+        Catch ex As Exception
+            MessageBox.Show("Invalid Function.", "Error")
+        End Try
+
+    End Sub
+
+    Private Sub Sort(ByVal Last As Array, ByVal First As Array, ByVal Middle As Array, ByVal Salary As Array)
+        'variables
+        Dim tempLast, tempFirst, tempMiddle As String
+        Dim k As Integer
+        Dim tempSalary As Double
+
+        'loop for length of entered values in array
+        For i As Integer = 1 To (intCount - 1)
+            'sets temp value to current array value
+            tempLast = arrLast(i)
+            tempFirst = arrFirst(i)
+            tempMiddle = arrMiddle(i)
+            tempSalary = arrSalary(i)
+            k = i - 1 'k equals i minus 1
+            'loop while k>= 0 and array(k) is further in alphabet then tempLast
+            While k >= 0 AndAlso arrLast(k) > tempLast
+                'sets array(k+1) to value of array(k)
+                arrLast(k + 1) = arrLast(k)
+                arrFirst(k + 1) = arrFirst(k)
+                arrMiddle(k + 1) = arrMiddle(k)
+                arrSalary(k + 1) = arrSalary(k)
+                k -= 1 'decriment k
+            End While
+            'sets array(k+1) to value stored in temp
+            arrLast(k + 1) = tempLast
+            arrFirst(k + 1) = tempFirst
+            arrMiddle(k + 1) = tempMiddle
+            arrSalary(k + 1) = tempSalary
+        Next
+
+    End Sub
+
+    Private Function FindHigh(ByRef Salary() As Double) As Double
+        Dim dblHigh As Double = Salary(0)
+        'loops for length of entered values in array
+        'compares first value to next and sets highest as dblHigh
+        'as loops, compares dblHigh to all other values and sets as necessary
+        For j As Integer = 0 To (intCount - 1)
+            If dblHigh < Salary(j) Then
+                dblHigh = Salary(j)
+            End If
+        Next
+        Return dblHigh
+    End Function
+
+    Private Function FindLow(ByRef Salary() As Double) As Double
+        Dim dblLow As Double = Salary(0)
+        'loops for length of entered values in array
+        'compares first value to next and sets lowest as dblLow
+        'as loops, compares dblLow to all other values and sets as necessary
+        For j As Integer = 0 To (intCount - 1)
+            If dblLow > Salary(j) Then
+                dblLow = Salary(j)
+            End If
+        Next
+        Return dblLow
+    End Function
+
+    Private Function PrintOutput(ByRef Last() As String, ByRef First() As String, ByRef Middle() As String,
+                                 ByRef Salary() As Double, ByRef Average As Double) As String
+        'builds string by adding new values as looping through array
+        'returns string with all values entered into array
+        Dim strOutput As String = "NAME".PadRight(25) & "SALARY".PadLeft(9) & "DEVIATION FROM AVERAGE".PadLeft(29) &
+            ControlChars.NewLine & ControlChars.NewLine
+
+        For j As Integer = 0 To (intCount - 1)
+            strOutput += (Last(j) & ", " & First(j) & " " & Middle(j)).PadRight(22) &
+                Salary(j).ToString("C2").PadLeft(15) & (Salary(j) - Average).ToString("C2").PadLeft(26) &
+                ControlChars.NewLine
+        Next
+        Return strOutput
+    End Function
+
+    Private Function OutputSalary(ByRef High As Double, ByRef Low As Double, ByRef Average As Double) As String
+        'builds string for high, low, and average salary; returns
+        Dim strOutput As String = "High Salary:".PadRight(17) & High.ToString("C2").PadLeft(15) & ControlChars.NewLine &
+            "Low Salary:".PadRight(17) & Low.ToString("C2").PadLeft(15) & ControlChars.NewLine &
+            "Average Salary:".PadRight(17) & Average.ToString("C2").PadLeft(15)
+        Return strOutput
+    End Function
+
+    'gets percent from text box
+    'recalculates stored salary values in array with percent
+    'displays new values in a different report
+    Private Sub btnNewReport_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNewReport.Click
+        'variables
+        Dim dblPercent, dblNewSum, dblNewHigh, dblNewLow, dblNewAverage As Double
+        Dim strPercent As String
+
+        Try
+            strPercent = txtBxPercent.Text 'get value from text box
+
+            'converts to decimal and adds 1
+            dblPercent = ((Convert.ToDouble(strPercent) / 100) + 1)
+
+            'loops through salary array and recalculates values
+            For j As Integer = 0 To (intCount - 1)
+                arrSalary(j) = arrSalary(j) * dblPercent
+                dblNewSum += arrSalary(j) 'running sum
+            Next
+
+            dblNewHigh = FindHigh(arrSalary) 'function finds highest salary
+            dblNewLow = FindLow(arrSalary) 'function finds lowest salary
+            dblNewAverage = dblNewSum / intCount 'calculates new average salary
+
+            'builds strings to print in new text boxes
+            txtBxNewHighLow.Text = "Salary Increase: " & strPercent & "%" & ControlChars.NewLine &
+                OutputSalary(dblNewHigh, dblNewLow, dblNewAverage)
+            txtBxNewOutput.Text = PrintOutput(arrLast, arrFirst, arrMiddle, arrSalary, dblNewAverage)
+
+            'sets for option to clear or exit
+            btnNewReport.Enabled = False
+            lblPercent.Enabled = False
+            txtBxPercent.Enabled = False
+            txtBxPercent.Clear()
+        Catch ex As Exception
+            MessageBox.Show("Invalid entry. Try again.", "Error")
+            txtBxPercent.Focus()
+        End Try
+
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        'clear arrays
+        Array.Clear(arrLast, 0, 49)
+        Array.Clear(arrFirst, 0, 49)
+        Array.Clear(arrMiddle, 0, 49)
+        Array.Clear(arrSalary, 0, 49)
+
+        'clear class variables
+        intCount = 0
+        dblSum = 0
+
+        'clear text boxes and status labels
+        txtBxHighLow.Clear()
+        txtBxNewHighLow.Clear()
+        txtBxOutput.Clear()
+        txtBxNewOutput.Clear()
+        lblNumRecords.Text = String.Empty
+
+        'enables buttons and text boxes
+        'read for new values to be added
+        btnAdd.Enabled = True
+        lblName.Enabled = True
+        txtBxName.Enabled = True
+        txtBxName.Focus()
+        lblSalary.Enabled = True
+        txtBxSalary.Enabled = True
+
+    End Sub
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Me.Close() 'exit application
+    End Sub
+
+    Private Sub txtBxName_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtBxName.KeyPress
+        'only allows alphabet, backspace, period, and space
+        If Not Char.IsLetter(e.KeyChar) AndAlso e.KeyChar <> ControlChars.Back AndAlso e.KeyChar <> "." AndAlso e.KeyChar <> " " Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtBxSalary_KeyPress(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtBxSalary.KeyPress,
+        txtBxPercent.KeyPress
+        'only allows numbers, backspace, and period
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> ControlChars.Back AndAlso e.KeyChar <> "." Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
